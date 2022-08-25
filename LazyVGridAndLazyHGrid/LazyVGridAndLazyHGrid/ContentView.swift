71aded645f819fb32148fca8d95334c759832c81
Asheesh Kumar Verma <asheesh.verma@payufin.com>
2022-08-25 13:33:17 +0530

    Created proj file

---
+++ LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyVGridAndLazyHGrid
+//
+//  Created by Asheesh Kumar Verma on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/LazyVGridAndLazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridAndLazyHGridApp.swift
+//  LazyVGridAndLazyHGrid
+//
+//  Created by Asheesh Kumar Verma on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridAndLazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
