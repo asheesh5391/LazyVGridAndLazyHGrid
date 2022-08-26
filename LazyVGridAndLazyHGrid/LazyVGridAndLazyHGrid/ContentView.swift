c9f11542144c7a739b8c057dff3d4244195b9efc
Asheesh Kumar Verma <asheesh.verma@payufin.com>
2022-08-26 11:26:12 +0530

    LazyVGrid

--- LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/ContentView.swift
+++ LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var flavors = [Flavor]()
+    
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible())]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(flavors, id: \.self) { item in
+                        VStack(spacing: 20) {
+                            Image(item.name)
+                            Text(item.name)
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .navigationBarTitle("Today's Flavors")
+        }
+        .onAppear(perform: getFlavors)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    func getFlavors() {
+        self.flavors = [Flavor(name: "Chocolate"),
+                        Flavor(name: "Vanilla"),
+                        Flavor(name: "Strawberry"),
+                        Flavor(name: "Mint Chip"),
+                        Flavor(name: "Pistachio")
+        ]
     }
 }
---
+++ LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/FlavorModel.swift
@@ -0,0 +1,12 @@
+//
+//  FlavorModel.swift
+//  LazyVGridAndLazyHGrid
+//
+//  Created by Asheesh Kumar Verma on 25/08/22.
+//
+
+import Foundation
+
+struct Flavor: Codable, Hashable {
+    var name: String
+}
